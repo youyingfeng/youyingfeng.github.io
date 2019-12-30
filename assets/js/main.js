eed9a8bf4ca00b340aa7543e956b6263a70b4790
You Yingfeng <42577406+youyingfeng@users.noreply.github.com>
2019-12-30 16:29:58 +0800

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+<script>
+function openModal(){
+  document.getElementById("modal").style.display = "block";
+}
+
+function closeModal(){
+  document.getElementById("modal").style.display = "none";
+}
+
+function currentSlide(n){
+
+}
+
+</script>
